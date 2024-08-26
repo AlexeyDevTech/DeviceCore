19a391fbb192bfbfbec4944e902c55820a7cdfe9
balmasov <balmasov@EC-240815>
2024-08-26 14:14:05 +0300

    work_in_progress: added voltageSyncroControllerDevice

--- DeviceCore/Program.cs
+++ DeviceCore/Program.cs
@@ -11,6 +11,9 @@ namespace DeviceCore
        
         static async Task Main(string[] args)
         {
+            var device = new MEADevice();
+            var dev = device.SelectDevice<CompensationControllerDevice>();
+            Console.WriteLine(dev.ToString());
             //var device = new PowerControllerDevice();
             //device.Connect();
 
