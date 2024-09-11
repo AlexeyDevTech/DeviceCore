fe52b58074256c5f126f818bbcfbfe27058a64e1
AlexeyDevTech <108923810+AlexeyDevTech@users.noreply.github.com>
2024-09-11 17:41:53 +0300

    data source

--- ANG24.Core.Devices/Base/DeviceBase.cs
+++ ANG24.Core.Devices/Base/DeviceBase.cs
@@ -1,276 +1,136 @@
-﻿using ANG24.Core.Devices.DeviceBehaviors.Interfaces;
-using ANG24.Core.Devices.Helpers;
-using ANG24.Core.Devices.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ANG24.Core.Devices.Base
 {
-    public abstract class DeviceBase : IDevice
+    public class DeviceBase
     {
-        //protected SerialPort _port;                                                       //непосредственно порт подключения
-        private Timer _reconnectTimer;                                          //таймер для попытки подключиться снова
-        public string _portName;                                              //именованный источник (имя порта)
-        private readonly IDeviceBehavior _behavior;                             //хендл поведения устройства
-        internal readonly ICommandBehavior _commandBehavior;                     //хендл поведения отправки команд устройством
-        public List<IOptionalBehavior> _optionalBehaviors;                     //опциональные хэндлы, определяющие специфику поведения конкретного устройства
-        List<ProcessAction> processedActions = new List<ProcessAction>();       //список действий-опций для пост-обработки (помимо основного действия)
-        public string DeviceStatus { get; } = "None";
-        public string DataBuffer { get; private set; } = string.Empty; //для хранения полученных сообщений по требованию    
-
-        //public event EventHandler? Connected;
-        //public event EventHandler? Disconnected;
-        //public event DREventHandler DeviceDataReceived;
-
-        //device online state
-        //public bool Online => _port?.IsOpen ?? false;
-
-        protected DeviceBase(IDeviceBehavior behavior, ICommandBehavior commandBeahvior)
-        {
-            _reconnectTimer = new Timer(Reconnect, null, Timeout.Infinite, Timeout.Infinite);
-            _optionalBehaviors = new List<IOptionalBehavior>();
-            _behavior = behavior;
-            _behavior.SetDevice(this);
-            _commandBehavior = commandBeahvior;
-            _commandBehavior.SetDevice(this);
-            //_port = new SerialPort();
-            //_port.DataReceived += OnDataReceived;
-        }
-
-        //protected void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
-        //{
-        //    if (_port.IsOpen)
-        //    {
-        //        try
-        //        {
-        //            while (_port.BytesToRead > 0) //пока в буфере количество байт не будет равно 0
-        //            {
-        //                string cur_data = _port.ReadLine().Replace('\r', ' ').Replace('\n', ' ').Trim();     //чтение по строке
-        //                ProcessData(cur_data);                  //в зависимости от реализации, вызываем пост-обработку данных 
-        //                _behavior.HandleData(cur_data);         //в зависимости от поведения -- производим обработку данных
-        //                _commandBehavior.HandleData(cur_data);  //для обработчика команд -- то же самое
-        //                if (_optionalBehaviors.Count > 0)
-        //                {
-        //                    foreach (var behavior in _optionalBehaviors)
-        //                    {
-        //                        behavior.HandleData(cur_data);
-        //                    }
-        //                }
-        //                //экспериментальная функция, выполняет дополнительные действия, добавляемые и включаемые по требованию
-        //                if (processedActions.Count > 0)
-        //                    foreach (ProcessAction action in processedActions)
-        //                    {
-        //                        action.Execute(cur_data);
-        //                        if (action.ExecutedOnce)
-        //                            processedActions.Remove(action);
-        //                    }
-        //                DeviceDataReceived?.Invoke(cur_data);
-        //            }
-        //        }
-        //        catch (InvalidOperationException)
-        //        {
-
-        //        }
-        //    }
-        //}
-        protected abstract void ProcessData(string data); //абстрактный метод для обработки полученных данных
-        //public virtual void SetCommand(string command)
-        //{
-        //    try
-        //    {
-        //        if (Online)
-        //        {
-        //            _port.Write(command); //попытка отправить данные
-        //            _behavior.RequestData(command);
-        //        }
-        //    }
-        //    catch (InvalidOperationException ioex)
-        //    {
-        //        //когда порт закрыт (да, такое бывает)
-        //        OnDisconnected();
-        //    }
-        //    catch (TimeoutException tex)
-        //    {
-        //        //когда время отправки команды истекло
-        //        //(или когда входной буфер занят)
-        //        Console.WriteLine("Timeout write command");
-        //        //ничего не делаем
-        //        //(технически, можно считать количество неудачных попыток)
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        //во всех остальных случаях 
-        //        //считаем, что устройство сломалось
-        //        OnDisconnected();
-        //    }
-        //}
-        protected void Execute(string command) => _commandBehavior.ExecuteCommand(command, null);
-        protected void Execute(string command, Func<bool>? predicate = null, Action? ifTrue = null, Action? ifFalse = null, CommandElementSettings? settings = null)
-        {
-            _commandBehavior.ExecuteCommand(command, predicate, ifTrue, ifFalse, settings);
-        }
-        protected void Execute(string command, Func<string, bool>? predicate = null, Action? ifTrue = null, Action? ifFalse = null, CommandElementSettings? settings = null)
-        {
-            _commandBehavior.ExecuteCommand(command, predicate, ifTrue, ifFalse, settings);
-        }
-        protected void Execute(string command, IOptionalCommandBehavior behavior, CommandElementSettings? settings = null) => _commandBehavior.ExecuteCommand(command, behavior, settings);
-
-        //#region Option management
-        //protected void Option(string Name, Action<string> action, bool isExecutedOnce = false, bool Active = true) => processedActions.Add(new ProcessAction
-        //{
-        //    Name = Name,
-        //    ProcessedAction = action,
-        //    ExecutedOnce = isExecutedOnce,
-        //    Usage = Active
-        //});
-        //protected void PredicatedOption(string Name, Func<string, bool> predicate, Action<string> action, bool isExecutedOnce = false, bool Active = true) => processedActions.Add(new PredicatedProcessAction
-        //{
-        //    Name = Name,
-        //    Predicate = predicate,
-        //    ProcessedAction = action,
-        //    ExecutedOnce = isExecutedOnce,
-        //    Usage = Active
-        //});
-        //protected void OptionClear() => processedActions.Clear();
-        //protected void OptionRemove(string Name)
-        //{
-        //    processedActions.Remove(processedActions.First(x => x.Name == Name));
-        //}
-        //protected void OptionOff(string Name)
-        //{
-        //    var r = processedActions.FirstOrDefault(x => x.Name == Name);
-        //    if (r != null) r.Usage = false;
-        //}
-        //protected void OptionOn(string Name)
-        //{
-        //    var r = processedActions.FirstOrDefault(x => x.Name == Name);
-        //    if (r != null) r.Usage = true;
-        //}
-        //#endregion
-
-        #region connection logic
-        //public virtual void Connect()
-        //{
-        //    try
-        //    {
-
-        //        //примитивная реализация подключения
-        //        if (!_port.IsOpen)
-        //        {
-        //            _port.Open();
-        //            OnConnected();
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        StartReconnectTimer();
-        //    }
-        //}
-        //public void Disconnect()
-        //{
-        //    //примитивная реализация отключения
-        //    try
-        //    {
-        //        if (_port.IsOpen)
-        //            _port.Close();
-        //        StopReconnectTimer();
-        //    }
-        //    catch
-        //    {
-        //        //обрабатывать нечего пока, поэтому пусто  (29.07)
-        //    }
-
-        //}
-        //protected async Task<bool> Find(string exceptedRequest, string exceptedResponce, int baudRate = 9600)
-        //{
-        //    var name = await SerialPortFinder.FindDeviceAsync(exceptedRequest, exceptedResponce, baudRate);
-        //    if (name == null) return false;
-        //    if (name.Contains("device not found")) return false;
-        //    _port.PortName = name;
-        //    _portName = name;
-        //    return true;
-        //}
-
-       // private void Reconnect(object? state) => Connect();
-        #endregion 
-
-        #region connect reactions
-        private void OnConnected()
-        {
-            Connected?.Invoke(this, EventArgs.Empty);
-            StopReconnectTimer();
-        }
-        private void OnDisconnected()
-        {
-            Disconnected?.Invoke(this, EventArgs.Empty);
-            StartReconnectTimer();
-        }
-        #endregion
-
-        #region service methods
-        internal void StartReconnectTimer() => _reconnectTimer.Change(2000, Timeout.Infinite);
-        internal void StopReconnectTimer() => _reconnectTimer.Change(Timeout.Infinite, Timeout.Infinite);
-        #endregion
-
-        public abstract void Ping();
+        IDataSource source;
     }
 
-    public class ProcessAction
+    public interface IDataSource
     {
-        public string Name { get; set; }
-        public Action<object> ProcessedAction { get; set; }
-        public bool ExecutedOnce { get; set; }
-        public bool Usage { get; set; }
+        string Name { get; }
+        Type DataReceivedType { get; set; }
+        event Action OnData;
+        T Read<T>();
+        void Write<T>(T data);
 
-        public virtual void Execute(object val)
+    }
+    public interface IDataSourceAdapter<T>
+    {
+        T Read();
+        void Write(T data);
+    }
+
+
+    //абстракция для обобщения устройств для COM порта 
+    public abstract class SerialAdapter<T> : IDataSourceAdapter<T>
+    {
+        protected SerialPort port;
+        protected SerialAdapter(SerialPort port)
         {
-            if (Usage)
-                ProcessedAction?.Invoke(val);
+          this.port = port;
+        }
+
+        public abstract T Read();
+
+        public abstract void Write(T data);
+    }
+    public class StringSerialAdapter : SerialAdapter<string>
+    {
+
+        public StringSerialAdapter(SerialPort port) : base(port) { }
+
+        public override string Read()
+        {
+           return port.ReadLine();
+        }
+
+        public override void Write(string data)
+        {
+            port.Write(data);
         }
     }
-    public class PredicatedProcessAction : ProcessAction
+    public class ByteSerialAdapter : SerialAdapter<byte[]>
     {
-        public Func<object, bool> Predicate { get; set; }
+        public ByteSerialAdapter(SerialPort port) : base(port) { }
 
-        public override void Execute(object val)
+        public override byte[] Read()
         {
-            if (Usage)
-                if (Predicate != null && Predicate.Invoke(val))
-                    base.Execute(val);
+
+            var arr = new byte[port.BytesToRead];
+            port.Read(arr, 0, arr.Length);
+            return arr;
+        }
+
+        public override void Write(byte[] data)
+        {
+            port.Write(data, 0, data.Length);
         }
     }
 
-    public static class DeviceBaseExtensions
+    public class SerialDataSource : DataSourceBase
     {
-        public static DeviceBase UseBehavior(this DeviceBase device, IOptionalBehavior behavior)
+        SerialPort port;
+        public string Name { get; }
+
+        public event Action OnData;
+
+        public SerialDataSource(SerialPort port) : base() 
         {
-            device._optionalBehaviors.Add(behavior);
-            behavior.SetDevice(device);
-            return device;
-        }
-        public static DeviceBase EnableBehavior(this DeviceBase device, string Name)
-        {
-            var item = device._optionalBehaviors.FirstOrDefault(x => x.Name == Name);
-            if (item != null) item.On();
-            return device;
-        }
-        public static DeviceBase DisableBehavior(this DeviceBase device, string Name)
-        {
-            var item = device._optionalBehaviors.FirstOrDefault(x => x.Name == Name);
-            if (item != null) item.Off();
-            return device;
-        }
-        public static T GetOptionalBehavior<T>(this DeviceBase device, string Name) where T : class, IOptionalBehavior
-        {
-            var item = device._optionalBehaviors.FirstOrDefault(x => x.Name == Name);
-            if (item != null)
+            this.port = port;
+            adapters = new Dictionary<Type, object>
             {
-                return item as T;
-            }
-            else
-            {
-                throw new NullReferenceException($"Object {nameof(T)} was not found!!!");
-            }
+                { typeof(string), new StringSerialAdapter(port) },
+                { typeof(byte[]), new ByteSerialAdapter(port) },
+            };
+        }
+        public SerialDataSource(string portName, int baudRate = 9600) : this(new SerialPort(portName, baudRate)) { }
+        public override T Read<T>()
+        {
+            return GetAdapter<T>().Read();
+        }
+
+        public override void Write<T>(T data)
+        {
+            GetAdapter<T>().Write(data);
         }
     }
+
+    public abstract class DataSourceBase : IDataSource
+    {
+        public string Name { get; }
+        public Type DataReceivedType { get; set; }
+
+        public event Action OnData;
+        protected Dictionary<Type, object> adapters;
+
+        protected DataSourceBase()
+        {
+          adapters = new Dictionary<Type, object>();
+        }
+
+        protected IDataSourceAdapter<T> GetAdapter<T>()
+        {
+            if (adapters.TryGetValue(typeof(T), out var adapter))
+            {
+                return (IDataSourceAdapter<T>)adapter;
+            }
+
+            throw new InvalidOperationException($"No adapter found for type {typeof(T).Name}");
+        }
+
+        public abstract T Read<T>();
+
+        public abstract void Write<T>(T data);
+    }
+
+
+
 }
-
