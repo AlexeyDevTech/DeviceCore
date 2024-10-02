669396144456cc274e3a8052e0365e7445b7d85b
AlexeyDevTech <108923810+AlexeyDevTech@users.noreply.github.com>
2024-10-02 17:37:46 +0300

    combobox red

--- TerminalLab/Controls/ViewModels/ControllerSettingsViewModel.cs
+++ TerminalLab/Controls/ViewModels/ControllerSettingsViewModel.cs
@@ -4,7 +4,13 @@ namespace TerminalLab.Controls.ViewModels
 {
     public class ControllerSettingsViewModel : BindableBase
     {
+        private int _baudRate;
 
+        public int BaudRate
+        {
+            get => _baudRate;
+            set => SetProperty(ref _baudRate, value);
+        }
         public ControllerSettingsViewModel()
         {
         }
