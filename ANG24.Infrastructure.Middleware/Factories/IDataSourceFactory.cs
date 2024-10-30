01816402031f2e4d6b0aeaf3abcfe5866038b52e
AlexeyDevTech <108923810+AlexeyDevTech@users.noreply.github.com>
2024-10-30 18:32:12 +0300

    в

---
+++ ANG24.Infrastructure.Middleware/Factories/IDataSourceFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANG24.Infrastructure.Middleware.Factories
+{
+    internal interface IDataSourceFactory
+    {
+    }
+}
